3c18c459e3a9a808ce672d4505bb1a9470f6187a
sachinolickal <sachin.olickan@gmail.com>
2018-07-31 01:02:32 +0530

    commited

---
+++ operators.py
@@ -0,0 +1,10 @@
+a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+sum = a+b
+multiplication = a*b
+difference = a-b
+division = a/b
+print("product of a and b = ",multiplication)
+print(difference)
+print(division)
+print (sum)
---
+++ twonumbers.py
@@ -0,0 +1,13 @@
+value1 = 10
+value2 = 20
+value3 = 30
+if value1 >= value2:
+    if value1 >= value3:
+        print ("greatest=",value1)
+    else:
+        print ("greatest=",value3)
+else:
+    if value2 >= value3:
+        print ("greatest=",value2)
+    else:
+        print ("greatest=",value3)
